94ddd18dcee4c6eb44eafa12e6c9a3c2a3600259
David Codner <dccodner24@gmail.com>
2022-11-11 12:17:54 -0600

    Added Code Bank

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,6 @@
 document.getElementById('top-card').onclick = function(){
     window.open("https://chewy441014.github.io/trail-picker/", '_blank');
 }
+document.getElementById('CodeBank').onclick = function(){
+    window.open("https://code-bank-bc.herokuapp.com/", '_blank');
+}
